de188c57b9523127f2b9ea8bf8c2a09e378ac3bc
jjgilces <jjgilces@espol.edu.ec>
2026-02-22 18:25:38 -0600

    JG: fix for big int

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import app from "./app.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+(BigInt.prototype as any).toJSON = function () {
+    return this.toString();
+};
 
 app.listen(PORT, () => {
     console.log(`Server running at port: ${PORT}`);
