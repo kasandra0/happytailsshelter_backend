e63fe3ef6465475c1555f2328484223d8c312075
schraderbachar <jj.schraderbachar@icloud.com>
2026-02-27 07:59:35 -0600

    updated inventory item

--- src/services/inventoryItem.service.ts
+++ src/services/inventoryItem.service.ts
@@ -1,30 +1,36 @@
 import prisma from "../lib/prisma.js";
 
 export async function getAllInventoryItems() {
-    return prisma.inventory_item.findMany();
+  return prisma.inventory_item.findMany({
+    orderBy: {
+      inventory_item_id: "asc",
+    },
+  });
 }
 
 export async function getInventoryItemsById(id: number) {
-    return prisma.inventory_item.findUnique({
-        where: { inventory_item_id: id },
-    });
+  return prisma.inventory_item.findUnique({
+    where: { inventory_item_id: id },
+  });
 }
 
 export async function createInventoryItems(data: any) {
-    return prisma.inventory_item.create({
-        data,
-    });
+  return prisma.inventory_item.create({
+    data,
+  });
 }
 
 export async function updateInventoryItem(id: number, data: any) {
-    return prisma.inventory_item.update({
-        where: { inventory_item_id: id }, data
-    });
+  //dev note- have to do this because it should have been @updated in prisma schema but it was @default(now) and that only gets set on creation- doesn't update.
+  data.lastupdated = new Date();
+  return prisma.inventory_item.update({
+    where: { inventory_item_id: id },
+    data,
+  });
 }
 
 export async function deleteInventoryItem(id: number) {
-    return prisma.inventory_item.delete({
-        where: { inventory_item_id: id },
-    });
+  return prisma.inventory_item.delete({
+    where: { inventory_item_id: id },
+  });
 }
-
