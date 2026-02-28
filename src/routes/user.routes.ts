927bc72895dc1deb66c8a0f3a9fc5d9b36a58e5b
hnabi94 <hnabi94@gmail.com>
2026-02-27 19:03:40 -0600

    Applied requireAuth middleware to /me route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller.js";
+import { requireAuth } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.get("/", userController.getAll);
-router.get("/me", userController.getMe);
+router.get("/me", requireAuth, userController.getMe);
 router.get("/:id", userController.getById);
 router.put("/:id", userController.updateUser);
 
