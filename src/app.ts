5a1f24d4138b3b2aa036a7a0f63b321d98521be6
vathral2620 <jim.liriano@gmail.com>
2026-03-04 14:34:19 -0500

    removing console errors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ import { setupSwagger } from "./swagger.js";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
