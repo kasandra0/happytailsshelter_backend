118858f4c1b93c3601825c1c94e8c6cbb530b84b
kasandra0 <44381697+kasandra0@users.noreply.github.com>
2026-03-31 21:49:46 -0600

    bug fix prisma generation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import dotenv from "dotenv";
 import { Pool } from "pg";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "../generated/prisma/client.js";
 
 dotenv.config();
 
